75121c72a90628d1ca773bb7bc46d5bdef7fb538
rcon1012 <rcon1012@gmail.com>
2016-04-03 23:42:59 -0400

    messages test data

--- src/GenerateTestData.java
+++ src/GenerateTestData.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class GenerateTestData {
 	public static void main(String args[]) {
 		try(BufferedWriter bw = new BufferedWriter(new FileWriter("../data.sql"));) {
+			//friendships
 			for(int i = 0; i < 200; i++) {
 				// allows for people to be friends with themselves
 				String friend1 = (int)(Math.random() * 99) + "";
@@ -23,10 +24,35 @@ public class GenerateTestData {
 				+ status + "\'" + ", "  + established + ");");
 				}
 				else {
-					bw.write("INSERT INTO FRIENDS VALUES(" + "\'" + friend1 + "\'" + ", " + "\'" + friend2 + "\'" + ", " + "\'" 
+					bw.write("INSERT INTO Friends VALUES(" + "\'" + friend1 + "\'" + ", " + "\'" + friend2 + "\'" + ", " + "\'" 
 				+ status + "\'" + ", "  + "\'" + established + "\'" + ");");
 				}
 				
+				bw.newLine();
+			}
+			// messages
+			for(int i = 0; i < 300; i++) {
+				// allows for people to be friends with themselves
+				String msgId = i + "";
+				String subject = "subject " + i;
+				String senderId = (new Random()).nextInt(100) + "";
+				int type = (new Random()).nextInt(2);
+				String recipId = null;
+				if(type == 0) {
+					recipId = (new Random()).nextInt(100) + "";
+				}
+				else {
+					recipId = (new Random()).nextInt(10) + "";
+				}
+				long offset = Timestamp.valueOf("2014-01-01 00:00:00").getTime();
+				long end = Timestamp.valueOf("2015-01-01 00:00:00").getTime();
+				long diff = end - offset + 1;
+				Timestamp timeSent = new Timestamp(offset + (long)(Math.random() * diff)); 
+				String msgTxt = "message " + i + " text";
+				
+				bw.write("INSERT INTO Messages VALUES(" + "\'" + msgId + "\'" + ", " + "\'" + subject + "\'" + ", " + "\'" +
+				senderId + "\'" + ", " + "\'" + recipId + "\'" + ", " + "\'" + timeSent + "\'" + ", " + "\'" +
+				msgTxt + "\'" + ", " + "\'" + (type+1) + "\'" + ");");
 				bw.newLine();
 			}
 			
